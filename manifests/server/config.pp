7d1fa6f6943ff5a78b3eead9f964b0bd36f2dc0d
Krishna Prajapati <krishna@olindata.com>
2015-01-28 11:45:55 +0100

    fixed conflict for merging with upstream

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -2,10 +2,6 @@
 class mysql::server::config {
   $options = $mysql::server::options
 
-  user { 'mysql': ensure => present, }
-
-  group { 'mysql': ensure => present, }
-
   File {
     owner  => 'root',
     group  => $mysql::server::root_group,
@@ -24,8 +20,6 @@ class mysql::server::config {
     purge   => $mysql::server::purge_conf_dir,
   }
 
-  if $mysql::server::manage_config_file {
-    file { $mysql::server::config_file:
       content => template('mysql/my.cnf.erb'),
       mode    => '0644',
     }
